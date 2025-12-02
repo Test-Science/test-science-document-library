2302a6ca91684be13edbeda8565b3ed112be6b24
John Haman <jhaman@ida.org>
2025-12-02 11:20:00 -0500

    AIR format

--- create-library.R
+++ create-library.R
@@ -4,21 +4,43 @@ library(here)
 library(data.table)
 library(tools)
 
+
 setwd("/mnt/c/Users/jhaman/Documents/test-science-documents-library/")
 here::i_am("create-library.R")
 
 dat <- read_xlsx("./test-science-documents-library.xlsx")
-num_posts <- nrow(dat); print(c("Number of posts:", num_posts))
+num_posts <- nrow(dat)
+print(c("Number of posts:", num_posts))
 
 ## Run this to create all the folders and slugs. Not Destructive.
 ## source("./create-project.R")
 
 ## Use blogdown to make new post folders and slugs
 ## stopped on 27
-posts <- list.files(here("content/post"), ".md$", recursive = TRUE, full.names = TRUE)
-posters <- list.files(here("content/post"), "poster", recursive = TRUE, full.names = TRUE)
-slides <- list.files(here("content/post"), "slide", recursive = TRUE, full.names = TRUE)
-papers <- list.files(here("content/post"), "paper", recursive = TRUE, full.names = TRUE)
+posts <- list.files(
+  here("content/post"),
+  ".md$",
+  recursive = TRUE,
+  full.names = TRUE
+)
+posters <- list.files(
+  here("content/post"),
+  "poster",
+  recursive = TRUE,
+  full.names = TRUE
+)
+slides <- list.files(
+  here("content/post"),
+  "slide",
+  recursive = TRUE,
+  full.names = TRUE
+)
+papers <- list.files(
+  here("content/post"),
+  "paper",
+  recursive = TRUE,
+  full.names = TRUE
+)
 
 
 for (i in 1:num_posts) {
@@ -26,15 +48,27 @@ for (i in 1:num_posts) {
   print(paste(i, ":", dat[i, ]$Project))
 
   ## Find the markdown file for each line
-  slug <- gsub("/index", "", blogdown:::trim_ws(blogdown:::post_filename(dat[i, ]$Project, "", "", dat[i, ]$Year)))
+  slug <- gsub(
+    "/index",
+    "",
+    blogdown:::trim_ws(blogdown:::post_filename(
+      dat[i, ]$Project,
+      "",
+      "",
+      dat[i, ]$Year
+    ))
+  )
 
   ## Find the paper, poster, and slide for this slug
-  post <- list.files(here(paste0("content/post/", slug)), ".md", full.names = TRUE)
+  post <- list.files(
+    here(paste0("content/post/", slug)),
+    ".md",
+    full.names = TRUE
+  )
   poster <- list.files(here(paste0("content/post/", slug)), "poster")
   slide <- list.files(here(paste0("content/post/", slug)), "slide")
   paper <- list.files(here(paste0("content/post/", slug)), "paper")
   cover <- list.files(here(paste0("content/post/", slug)), "cover")
-
 
   ## Create project data
   project_data <- sprintf(
@@ -74,13 +108,34 @@ dat[i, ]$Year, # Date, for the headers
 dat[i, ]$Year, # Year, for taxonomies
 
 # Research Area
-paste0("[", paste(paste0( "'", strsplit(dat[i, ]$Area, split = ", ")[[1]], "'"), collapse = ","), "]"),
+paste0(
+  "[",
+  paste(
+    paste0("'", strsplit(dat[i, ]$Area, split = ", ")[[1]], "'"),
+    collapse = ","
+  ),
+  "]"
+),
 
 # Keywords, Convert a, b, c to ['a', 'b', 'c']
-paste0("[", paste(paste0( "'", strsplit(dat[i, ]$Keywords, split = ", ")[[1]], "'"), collapse = ","), "]"),
+paste0(
+  "[",
+  paste(
+    paste0("'", strsplit(dat[i, ]$Keywords, split = ", ")[[1]], "'"),
+    collapse = ","
+  ),
+  "]"
+),
 
 # Researchers for taxonomy
-paste0("[", paste(paste0( "'", strsplit(dat[i, ]$Authors, split = ", ")[[1]], "'"), collapse = ","), "]"),
+paste0(
+  "[",
+  paste(
+    paste0("'", strsplit(dat[i, ]$Authors, split = ", ")[[1]], "'"),
+    collapse = ","
+  ),
+  "]"
+),
 
 # Audience
 dat[i, ]$Audience,
@@ -93,47 +148,49 @@ dat[i, ]$Journal,
 ##        paste0( "\"", cover, "\""),
 ##        ""),
 
-
 ### Content
 
 # Video, if present
-ifelse(!is.na(dat[i, ]$Video),
-       paste("{{< youtube id=",
-             ## Extract the video ID from the youtube URL
-             regmatches(dat[i, ]$Video, regexpr("(?<=watch\\?v=).*$", dat[i, ]$Video, perl = TRUE)),
-             ">}}"),
-       ""),
+ifelse(
+  !is.na(dat[i, ]$Video),
+  paste(
+    "{{< youtube id=",
+    ## Extract the video ID from the youtube URL
+    regmatches(
+      dat[i, ]$Video,
+      regexpr("(?<=watch\\?v=).*$", dat[i, ]$Video, perl = TRUE)
+    ),
+    ">}}"
+  ),
+  ""
+),
 
 # Abstract, if present
-ifelse(!is.na(dat[i, ]$Description),
-       paste(dat[i, ]$Description),
-       ""),
+ifelse(!is.na(dat[i, ]$Description), paste(dat[i, ]$Description), ""),
 
 # Suggest Citation
 dat[i, ]$`Suggested-Citation`,
 
-
 # Slides
-ifelse(!identical(slide, character(0)),
-       paste0("#### Slides: ",
-              "\n",
-              "{{< pdfReader \"", slide, "\" >}}"),
-       ""),
-
+ifelse(
+  !identical(slide, character(0)),
+  paste0("#### Slides: ", "\n", "{{< pdfReader \"", slide, "\" >}}"),
+  ""
+),
 
 # Paper
-ifelse(!identical(paper, character(0)),
-       paste0("#### Paper: ",
-              "\n",
-              "{{< pdfReader \"", paper, "\" >}}"),
-       ""),
+ifelse(
+  !identical(paper, character(0)),
+  paste0("#### Paper: ", "\n", "{{< pdfReader \"", paper, "\" >}}"),
+  ""
+),
 
 # Poster
-ifelse(!identical(poster, character(0)),
-       paste0("#### Poster: ",
-              "\n",
-              "{{< pdfReader \"", poster, "\" >}}"),
-       "")
+ifelse(
+  !identical(poster, character(0)),
+  paste0("#### Poster: ", "\n", "{{< pdfReader \"", poster, "\" >}}"),
+  ""
+)
 )
 
   ## Write to each file
